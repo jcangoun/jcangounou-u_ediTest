57fd45bfa475a56aff10ecd9a9b0920ff60aee0f
ANGOUNOU <jcangounou@hotmail.fr>
2026-03-10 19:39:59 +0100

    save

---
+++ frontEnd/index.js
@@ -0,0 +1,7 @@
+// window.location()
+
+const enTete = document.querySelector("header");
+enTete.setAttribute = ('id', 'lentete');
+const navette = document.querySelector("nav");
+console.log(navette);
+console.log(enTete);
