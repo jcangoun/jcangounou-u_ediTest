b45330760a7345b82c9c74fd3f16ce2ca63ea4b2
ANGOUNOU <jcangounou@hotmail.fr>
2026-03-14 10:23:10 +0100

    back save

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,7 +1,7 @@
 // window.location()
 const body = document.querySelector("body");
 const enTete = document.querySelector("header");
-console.log(body);
+// console.log(body);
 body.appendChild(enTete);
 // enTete.setAttribute = ('id', 'lentete');
 enTete.id = ('lentete');
